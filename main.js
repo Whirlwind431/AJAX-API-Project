eda2699ec9fcc26b13e89a270b7f64182121c3da
Whirlwind431 <nikishovvladislav1@gmail.com>
2023-11-10 20:31:00 +0200

    more info btn + dropdown menu (css + js)

--- main.js
+++ main.js
@@ -44,36 +44,68 @@
     // display all coins on the screen
     function displayAllCoins(coins) {
         const divResponse = document.getElementById("cardsResponse")
-        for (let i = 0; i < 100; i++) {
+        for (let i = 1; i <= 100; i++) {
             const index = coins.indexOf(coins[i])
             const divElement = document.createElement('div')
             const coinName = document.createElement('h3')
             const coinSymbol = document.createElement('p')
             const coinImg = document.createElement('img')
             const brakeEl = document.createElement('br')
-            const moreInfoBrn = document.createElement('button')
+            const moreInfoBtn = document.createElement('button')
 
             // css 
             divElement.setAttribute("class", "coinCard")
             coinImg.setAttribute("src", `${coins[i].image}`)
             coinImg.setAttribute("class", "myImgDiv")
 
-
             // set data into cells
             coinName.innerHTML = `${coins[i].name}`
             coinSymbol.innerHTML = `${coins[i].symbol}`
-            moreInfoBrn.innerHTML = "More info"
+            moreInfoBtn.innerHTML = "More info"
             // buttons
-            moreInfoBrn.addEventListener("click", function () {
-                moreInfo(index)
+            moreInfoBtn.addEventListener("click", function () {
+                myFunction(index)
             })
+
+
+            // dropdown menu
+            const divDropdown = document.createElement('div')
+            const interiorDiv = document.createElement('div')
+            const linkA = document.createElement('a')
+            const linkB = document.createElement('a')
+            const linkC = document.createElement('a')
+
+            // css 
+            divDropdown.setAttribute("class", "dropdown")
+            interiorDiv.setAttribute("class", "dropdown-content")
+            interiorDiv.setAttribute("id", "myDropdown")
+
+            // Add a unique ID to each dropdown to associate with the button
+            interiorDiv.setAttribute("id", `myDropdown${i}`);
+            // Add a unique ID to each button to associate with the dropdown
+            moreInfoBtn.setAttribute("data-dropdown", `myDropdown${i}`);
+
+            moreInfoBtn.setAttribute("class", "dropbtn")
+            linkA.innerHTML = `Price:${(coins[i].current_price).toFixed(2)}$`
+            linkB.innerHTML = `Price:${(coins[i].current_price / 1.07).toFixed(2)}€`
+            linkC.innerHTML = `Price:${(coins[i].current_price / 0.26).toFixed(2)}₪`
+
+
 
             // append elements to our main divCards
             divElement.appendChild(coinName)
             divElement.appendChild(coinSymbol)
             divElement.appendChild(coinImg)
             divElement.appendChild(brakeEl)
-            divElement.appendChild(moreInfoBrn)
+
+            divDropdown.appendChild(moreInfoBtn)
+            interiorDiv.appendChild(linkA)
+            interiorDiv.appendChild(linkB)
+            interiorDiv.appendChild(linkC)
+            divDropdown.appendChild(interiorDiv)
+            divElement.appendChild(divDropdown)
+
+            // append to main div
             divResponse.appendChild(divElement)
 
         }
@@ -106,13 +138,28 @@
     document.addEventListener('DOMContentLoaded', onload)
     // ---------------------------------------------------------
 
-
-
-
-    function moreInfo(index) {
-
-        console.log(`clicked element ${index}`);
+    // Dropdown menu 
+    // When the user clicks on the button,
+    // toggle between hiding and showing the dropdown content 
+    function myFunction(index) {
+        const dropdown = document.getElementById(`myDropdown${index}`);
+        dropdown.classList.toggle("show");
     }
+
+    // Close the dropdown menu if the user clicks outside of it
+    window.onclick = function (event) {
+        if (!event.target.matches('.dropbtn')) {
+            var dropdowns = document.getElementsByClassName("dropdown-content");
+            var i;
+            for (i = 0; i < dropdowns.length; i++) {
+                var openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')) {
+                    openDropdown.classList.remove('show');
+                }
+            }
+        }
+    }
+
 
 
 
